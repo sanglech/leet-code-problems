534554c5fc2a6ed910abcfd07677fb6b8070d5cd
Christian <christian@sanglech.com>
2022-10-10 00:52:16 +0900

    insert into BST

---
+++ src/LinkedList/InsertIntoBST.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class InsertIntoBST {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode current = root;
+        TreeNode prev = null;
+        TreeNode newNode = new TreeNode(val);
+
+        if(root==null) return newNode;
+
+        while(current!=null){
+            prev= current;
+            if(current.val<val){
+                current = current.right;
+            }
+            else {
+                current = current.left;
+            }
+        }
+        if(prev.val > val){
+            prev.left = newNode;
+        }
+        else {
+            prev.right = newNode;
+        }
+        return root;
+    }
+}
