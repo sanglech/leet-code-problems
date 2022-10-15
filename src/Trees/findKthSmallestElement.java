94c6abbc51ebae7df8acda0ac00a2bf556fd69f6
Christian <christian@sanglech.com>
2022-10-15 20:48:51 +0900

    add find kth smallest element

---
+++ src/Trees/findKthSmallestElement.java
@@ -0,0 +1,19 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findKthSmallestElement {
+    List<Integer> sortedArr = new ArrayList<>();
+    public int kthSmallest(TreeNode root, int k) {
+        inorderTraverse(root);
+        return sortedArr.get(k-1);
+    }
+
+    void inorderTraverse(TreeNode root) {
+        if (root == null) return;
+        inorderTraverse(root.left);
+        sortedArr.add(root.val);
+        inorderTraverse(root.right);
+    }
+}
