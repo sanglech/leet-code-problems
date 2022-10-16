d0d72d222aea4e85e22930854fe82295d6cbd24b
Christian <christian@sanglech.com>
2022-10-16 21:42:53 +0900

    added path sum

---
+++ src/Trees/PathSum.java
@@ -0,0 +1,20 @@
+package Trees;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        int val =0;
+        return dfs(root,val,targetSum);
+    }
+
+    public boolean dfs(TreeNode current, int currentSum, int targetSum){
+        if(current ==null) return false;
+        currentSum+=current.val;
+
+        if(current.left==null && current.right==null){
+            return currentSum == targetSum;
+        }
+
+        return (dfs(current.left,currentSum, targetSum) || dfs(current.right,currentSum, targetSum));
+
+    }
+}
