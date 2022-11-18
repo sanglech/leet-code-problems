645f346771622f83a70e80267171ad74f618df40
Christian <christian@sanglech.com>
2022-11-18 14:17:23 +0900

    add two sum solution for python

---
+++ python/1-Two-Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        remainderMap={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in remainderMap:
+                return [remainderMap[diff],i]
+            remainderMap[num]=i
