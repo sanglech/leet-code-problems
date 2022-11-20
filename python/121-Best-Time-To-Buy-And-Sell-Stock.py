12dc4b29a0e365af9d6b20661a2b9b4cec82058d
Christian <christian@sanglech.com>
2022-11-20 20:14:43 +0900

    best time to buy and sell stock python

---
+++ python/121-Best-Time-To-Buy-And-Sell-Stock.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+         if len(prices) == 1:
+             return 0
+         l=0
+         maxProfit = 0
+
+         for r, price in enumerate(prices):
+            currProfit = price - prices[l]
+            maxProfit = max(maxProfit,currProfit)
+            if (currProfit < 0):
+                l=r 
+         return maxProfit
