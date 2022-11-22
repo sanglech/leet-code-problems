582ef41d6c9f49612bb72f511906c7467ecd9f6a
Christian <christian@sanglech.com>
2022-11-22 16:39:39 +0900

    add LC number

--- java/src/BinarySearch/GuessGame.java
+++ java/src/BinarySearch/GuessGame.java
@@ -1,16 +1,9 @@
 package BinarySearch;
 
+/*
+    374. Guess Number Higher or Lower
+ */
 public class GuessGame {
-    /**
-     * Forward declaration of guess API.
-     * @param  num   your guess
-     * @return 	     -1 if num is higher than the picked number
-     *			      1 if num is lower than the picked number
-     *               otherwise return 0
-     * int guess(int num);
-     */
-
-    public class Solution extends GuessGame {
         public int guessNumber(int n) {
             int left = 1;
             int right =n;
@@ -31,7 +24,6 @@ public class GuessGame {
 
             return -1;
         }
-    }
 
     public int guess( int num){
         return 1;
