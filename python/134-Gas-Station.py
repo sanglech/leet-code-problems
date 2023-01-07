82bd576af7a174c4a32827afde1c2ef458655f60
Christian <christian@sanglech.com>
2023-01-07 20:42:05 +0900

    solutions for gas station problem

---
+++ python/134-Gas-Station.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(cost) > sum(gas):
+            return -1
+        res = 0
+        total = 0
+
+        for i in range(len(gas)):
+            total+=gas[i]-cost[i]
+
+            if(total<0):
+                total = 0
+                res = i+1
+        return res
