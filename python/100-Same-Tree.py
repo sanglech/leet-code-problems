9070d3b5450f449167296e18cf80b663d6995f45
Christian <christian@sanglech.com>
2023-01-10 19:45:16 +0900

    python solution for same tree

---
+++ python/100-Same-Tree.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from TreeNode import TreeNode
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if(p is None or q is None or p.val!=q.val):
+            return False
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right))
